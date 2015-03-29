cfd1b13aeb71cb6e1f96e8160c54904ef6a1fa45
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2015-03-29 22:40:33 +0300

    Update #1

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Project Overview
 
-In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
-
+In this project I got a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and application development now left over with an incomplete test suite. That's where I come in.
 
 ## Why this Project?
 
@@ -10,7 +9,7 @@ Testing is an important part of the development process and many organizations p
 Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
 
 
-## What will I learn?
+## What did I learn?
 
 You will learn how to use Jasmine to write a number of tests against a pre-existing application. These will test the underlying business logic of the application as well as the event handling and DOM manipulation.
 
@@ -21,7 +20,7 @@ You will learn how to use Jasmine to write a number of tests against a pre-exist
 * Good tests give you the ability to quickly analyze whether new code breaks an existing feature within your codebase, without having to manually test all of the functionality.
 
 
-# How will I complete this project?
+# How my Supervisor told me to complete this project?
 
 1. Download the [required project assets](http://github.com/udacity/frontend-nanodegree-feedreader).
 2. Review the functionality of the application within your browser.
@@ -37,3 +36,27 @@ You will learn how to use Jasmine to write a number of tests against a pre-exist
 12. Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container. Remember, loadFeed() is asynchronous so this test wil require the use of Jasmine's beforeEach and asynchronous done() function.
 13. Write a test that ensures when a new feed is loaded by the loadFeed function that the content actually changes. Remember, loadFeed() is asynchronous.
 14. When complete - all of your tests should pass.
+
+# How really I did it?
+
+First of all, I took my supervisor plan as a start point and my steps goes as the following:
+1. Looking at application behavior.
+2. Browse HTML code page.
+3. Browse CSS Style sheet.
+4. Browse JavaScript Files.
+5. Browse the [Jasmine](http://jasmine.github.io/) Documentations.
+6. Browse [Jasmine](http://jasmine.github.io/) source code.
+7. Search for TODO section and understand the requirements.
+8. Start writing description sections.
+9. Start writing It sections in description sections.
+10. write test as [Jasmine](http://jasmine.github.io/) Doc says.
+11. In some tests I did HTML/CSS analyse.
+12. In some tests I make things to fail in order to check if [Jasmine](http://jasmine.github.io/) working properly.
+13. Write comments to describe what I did in the tests code.
+14. Complete this README.md
+
+## Licence
+
+It's Completely Free. But, Do whatever you like to do on your own full responsibility;
+
+This licence is known with [MIT License](http://vzool.mit-license.org/) in professional networks.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,16 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+ 
 $(function() {
+
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
+
     describe('RSS Feeds', function() {
+
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,8 +25,11 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
+
         it('are defined', function() {
+            // check if allFeeds is defined
             expect(allFeeds).toBeDefined();
+            // check if allFeeds array not empty
             expect(allFeeds.length).not.toBe(0);
         });
 
@@ -32,15 +39,49 @@ $(function() {
          * and that the URL is not empty.
          */
 
+        it('url should be not empty', function(){
+            allFeeds.forEach(function(item){
+                // check if url is defined
+                expect(item.url).not.toBeUndefined();
+                expect(item.url).not.toBeNaN();
+                // check if url is not null
+                expect(item.url).not.toBeNull();
+                // check if url is not empty string
+                expect(item.url).not.toEqual('');
+                // check if url is valid
+                expect(/^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/.test(item.url)).toBe(true);
+            });
+        });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('name should be not empty', function(){
+            allFeeds.forEach(function(item){
+                // check if name is defined
+                expect(item.name).not.toBeUndefined();
+                expect(item.name).not.toBeNaN();
+                // check if name is not null
+                expect(item.name).not.toBeNull();
+                // check if name is not empty string
+                expect(item.name).not.toEqual('');
+            });
+        });
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+
+    describe('The menu', function(){
+
+        beforeEach(function() {
+            // install clock for delayed checks
+            jasmine.clock().install();
+        });
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -48,13 +89,50 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
+        it('should hidden by default', function(){
+            
+            // default value when menu is hidden
+            expect($(".menu").css('transform')).toBe('matrix(1, 0, 0, 1, -192, 0)');
+
+        });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+        it('should visibility on/off on click', function(){
+            
+            // default value when menu is hidden
+            expect($(".menu").css('transform')).toBe('matrix(1, 0, 0, 1, -192, 0)');
+            
+            // fire press action on menu icon programmatically
+            $(".icon-list").click();
+
+            // wait 50ms to click event take effect
+            etTimeout(function(){
+                // default value when menu is shown
+                expect($(".menu").css('transform')).toBe('matrix(1, 0, 0, 1, 0, 0)');
+            }, 50);
+
+            // fire press action on menu icon programmatically
+            $(".icon-list").click();
+
+            // wait 50ms to click event take effect
+            setTimeout(function(){
+                // default value when menu is hidden
+                expect($(".menu").css('transform')).toBe('matrix(1, 0, 0, 1, -192, 0)');
+            }, 50);
+
+        });
+
+    });
+
+
     /* TODO: Write a new test suite named "Initial Entries" */
+
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,10 +141,69 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        // pointer to this inside function inside this scope
+        var self = this;
+
+        // condition trigger
+        self.InitialComplete = false;
+
+        beforeEach(function(done){
+            
+            // load feed
+            loadFeed(0, function(){
+                // condition test
+                self.InitialComplete = $('.feed .entry').length >= 1;
+                // trigger finish function call
+                done();
+            });
+
+        });
+
+        it('should load at least one feed', function(done){
+            // check if InitialComplete is true
+            expect(self.InitialComplete).toBe(true);
+            // trigger finish function call
+            done();
+        });
+        
+    });
+
+
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+
+    describe('New Feed Selection', function(){
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+        // pointer to this inside function inside this scope
+        var self = this;
+
+        // condition trigger
+        self.InitialComplete = false;
+        // first feed count before call loadFeed
+        self.count_before_loadfeed = $('.feed .entry').length;
+
+        beforeEach(function(done){
+
+            loadFeed(0, function(){
+                // check if entry count is changed and set in InitialComplete
+                self.InitialComplete = $('.feed .entry').length != self.count_before_loadfeed;
+                // trigger finish function call
+                done();
+            });
+
+        });
+
+        it('should change its content when new data loaded', function(done){
+            // check if InitialComplete is true
+            expect(self.InitialComplete).toBe(true);
+            // trigger finish function call
+            done();
+        });
+
+    });
+
 }());
